acf5a0728a85ee7fc8e7f7fcdcea55efdbbfc117
vcarenet <vcare.net23@gmail.com>
2022-02-25 22:33:49 +0530

    updated form and php

--- sendemail.php
+++ sendemail.php
@@ -1,17 +1,57 @@
 <?php
-$name       = @trim(stripslashes($_POST['name'])); 
-$from       = @trim(stripslashes($_POST['email'])); 
-$subject    = @trim(stripslashes($_POST['subject'])); 
-$message    = @trim(stripslashes($_POST['message'])); 
-$to   		= 'vcare.net23@gmail.com';//replace with your email
-
-$headers   = array();
-$headers[] = "MIME-Version: 1.0";
-$headers[] = "Content-type: text/plain; charset=iso-8859-1";
-$headers[] = "From: {$name} <{$from}>";
-$headers[] = "Reply-To: <{$from}>";
-$headers[] = "Subject: {$subject}";
-$headers[] = "X-Mailer: PHP/".phpversion();
-
-mail($to, $subject, $message, $headers);
+  
+if($_POST) {
+    $visitor_name = "";
+    $visitor_email = "";
+    $email_title = "";
+    $visitor_message = "";
+    $email_body = "<div>";
+      
+    if(isset($_POST['name'])) {
+        $visitor_name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Visitor Name:</b></label>&nbsp;<span>".$visitor_name."</span>
+                        </div>";
+    }
+ 
+    if(isset($_POST['email'])) {
+        $visitor_email = str_replace(array("\r", "\n", "%0a", "%0d"), '', $_POST['email']);
+        $visitor_email = filter_var($visitor_email, FILTER_VALIDATE_EMAIL);
+        $email_body .= "<div>
+                           <label><b>Visitor Email:</b></label>&nbsp;<span>".$visitor_email."</span>
+                        </div>";
+    }
+      
+    if(isset($_POST['subject'])) {
+        $email_title = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+        $email_body .= "<div>
+                           <label><b>Reason For Contacting Us:</b></label>&nbsp;<span>".$email_title."</span>
+                        </div>";
+    }
+  
+    if(isset($_POST['message'])) {
+        $visitor_message = htmlspecialchars($_POST['message']);
+        $email_body .= "<div>
+                           <label><b>Visitor Message:</b></label>
+                           <div>".$visitor_message."</div>
+                        </div>";
+    }
+      
+    $recipient = "vcare.net23@gmail.com";
+      
+    $email_body .= "</div>";
+ 
+    $headers  = 'MIME-Version: 1.0' . "\r\n"
+    .'Content-type: text/html; charset=utf-8' . "\r\n"
+    .'From: ' . $visitor_email . "\r\n";
+      
+    if(mail($recipient, $email_title, $email_body, $headers)) {
+        echo "<p>Thank you for contacting us, $visitor_name. You will get a reply within 24 hours.</p>";
+    } else {
+        echo '<p>We are sorry but the email did not go through.</p>';
+    }
+      
+} else {
+    echo '<p>Something went wrong</p>';
+}
 ?>
